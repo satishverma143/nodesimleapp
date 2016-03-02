6dc0533f2c60385bf1d5ff4b0338e881aee35279
satish.verma143 <satish.verma143@gmail.com>
2016-03-02 15:50:17 +0530

    'message'

---
+++ server.js
@@ -0,0 +1,78 @@
+// var mysql = require('mysql')
+// // Let’s make node/socketio listen on port 3000
+// var io = require('socket.io').listen(3000)
+var port = 9988;
+var express = require('express'),
+  app = express().use(express.static(__dirname + '/')),
+  http = require('http').Server(app),
+  //server = http.createServer(app),
+  //,server = connect().use(connect.static(__dirname + '/'))
+  //io = require('socket.io').listen(server),
+  io = require('socket.io')(http),
+  bodyParser = require('body-parser'),
+  util = require('util'),
+  mysql = require('mysql');
+
+//server.listen(9988);
+http.listen(port, function() {
+  console.log("server started on port : " + port);
+})
+// Define our db creds
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'nodechat'
+})
+
+// Log any errors connected to the db
+db.connect(function (err) {
+  if(err) console.log(err);
+})
+
+// Define/initialize our global vars
+var notes = []
+var isInitNotes = false
+var socketCount = 0
+
+io.sockets.on('connection', function(socket) {
+  // Socket has connected, increase socket count
+  socketCount++
+  // Let all sockets know how many are connected
+  io.sockets.emit('users connected', socketCount)
+
+  socket.on('disconnect', function() {
+    // Decrease the socket count on a disconnect, emit
+    socketCount--
+    io.sockets.emit('users connected', socketCount)
+  })
+
+  socket.on('new note', function(data){
+        // New note added, push to all sockets and insert into db
+        notes.push(data)
+        io.sockets.emit('new note', data)
+        // Use node's db injection format to filter incoming data
+        db.query('INSERT INTO notes (note) VALUES (?)', data.note)
+    })
+
+    // Check to see if initial query/notes are set
+    if (! isInitNotes) {
+        // Initial app start, run db query
+        db.query('SELECT * FROM notes')
+            .on('result', function(data){
+                // Push results onto the notes array
+                notes.push(data)
+            })
+            .on('end', function(){
+                // Only emit notes after query has been completed
+                socket.emit('initial notes', notes)
+            })
+
+        isInitNotes = true
+    } else {
+        // Initial notes already exist, send out
+        socket.emit('initial notes', notes)
+    }
+
+
+
+})
